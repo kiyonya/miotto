5416afbe4b89959329473602bdd8537f70dd1070
kiyonya <481366085@qq.com>
2026-02-26 14:43:29 +0800

    feat: 窗口统一控制

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,10 +11,13 @@ import { ncmOrpheus } from './ipc/orpheus'
 import { DataTransport } from './ipc/transport'
 import { OSCIPC } from './ipc/osc'
 import { MediaIpc } from './ipc/media'
+import { pluginIPC } from './ipc/plugin'
+import { windowManager } from './window'
+import { controlIPC } from './ipc/control'
 
-function createWindow(): void {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+function startAPP(){
+
+  const mainWindow = windowManager.createWindow('main',{
     width: 1060,
     height: 720,
     show: false,
@@ -28,9 +31,6 @@ function createWindow(): void {
       webSecurity: false
     }
   })
-
-  app.commandLine.appendSwitch('js-flags', '--expose-gc');
-
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
@@ -39,69 +39,43 @@ function createWindow(): void {
     shell.openExternal(details.url)
     return { action: 'deny' }
   })
-
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
-
   appIpc(mainWindow)
   OSCIPC(mainWindow)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
+  startAPP()
   electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
 
   registerNCMApiIPC()
   biliApi()
   localAPI()
   CacheIPC()
   ncmOrpheus()
-  createWindow()
   DataTransport()
   MediaIpc()
-
-  console.log(app.getPath('appData'))
-
+  pluginIPC()
+  controlIPC()
 
   session.defaultSession.setDisplayMediaRequestHandler((request, callback) => {
     desktopCapturer.getSources({ types: ['screen'] }).then((sources) => {
       callback({ video: sources[0], audio: 'loopback' })
     })
   }, { useSystemPicker: true })
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
---
+++ src/main/window.ts
@@ -0,0 +1,132 @@
+import { optimizer } from "@electron-toolkit/utils";
+import { BrowserWindow } from "electron";
+import EventEmitter from "events";
+
+export class WindowManager<AvailableIds extends Array<string | number> = Array<string | number>, AvailableGroupIds extends Array<string> = Array<string>> extends EventEmitter {
+    private windows = new Map<AvailableIds[number], BrowserWindow>()
+    private groups: Record<AvailableGroupIds[number], Set<BrowserWindow>> = {} as any
+    constructor() {
+        super()
+    }
+    public createWindow(id: AvailableIds[number], options?: Electron.BaseWindowConstructorOptions & Electron.BrowserViewConstructorOptions, groupId?: AvailableGroupIds[number]): BrowserWindow {
+        if (this.windows.has(id)) {
+            throw new Error(`Window with id "${id}" already exists`)
+        }
+        const window = new BrowserWindow(options)
+        this.windows.set(id, window)
+
+        if (groupId) {
+            if (!this.groups[groupId]) {
+                this.groups[groupId] = new Set<BrowserWindow>([window])
+            }
+            else {
+                this.groups[groupId].add(window)
+            }
+        }
+
+        window.on('closed', () => {
+            this.windows.delete(id)
+            if (groupId) {
+                this.groups[groupId]?.delete(window)
+            }
+        })
+
+        window.on('show', () => {
+            window.webContents.send('win:created', window.webContents.id)
+        })
+
+        optimizer.watchWindowShortcuts(window)
+
+        return window
+    }
+
+    public getWindow(id: AvailableIds[number]): BrowserWindow | undefined {
+        return this.windows.get(id)
+    }
+
+    public getGroupWindows(groupId: AvailableGroupIds[number]) {
+        return this.groups[groupId] ? Array.from(this.groups[groupId]) : []
+    }
+
+    public closeWindow(id: AvailableIds[number]): void {
+        const win = this.windows.get(id)
+        if (win) {
+            win.close()
+        }
+    }
+
+    public closeAllWindows(): void {
+        this.windows.forEach(win => win.close())
+        this.windows.clear()
+    }
+
+    public getAllWindows(): IterableIterator<BrowserWindow> {
+        return this.windows.values()
+    }
+
+    public hasWindow(id: AvailableIds[number]): boolean {
+        return this.windows.has(id)
+    }
+
+    public minimizeWindow(id: AvailableIds[number]): void {
+        const win = this.windows.get(id)
+        if (win && !win.isMinimized()) {
+            win.minimize()
+        }
+    }
+
+    public maximizeWindow(id: AvailableIds[number]): void {
+        const win = this.windows.get(id)
+        if (win && !win.isMaximized()) {
+            win.maximize()
+        }
+    }
+
+    public restoreWindow(id: AvailableIds[number]): void {
+        const win = this.windows.get(id)
+        if (win && win.isMinimized()) {
+            win.restore()
+        }
+    }
+
+    public hideWindow(id: AvailableIds[number]): void {
+        const win = this.windows.get(id)
+        if (win && !win.isDestroyed()) {
+            win.hide()
+        }
+    }
+
+    public showWindow(id: AvailableIds[number]): void {
+        const win = this.windows.get(id)
+        if (win && !win.isDestroyed()) {
+            win.show()
+        }
+    }
+
+    public focusWindow(id: AvailableIds[number]): void {
+        const win = this.windows.get(id)
+        if (win && !win.isDestroyed()) {
+            win.focus()
+        }
+    }
+
+    public getWindowCount(): number {
+        return this.windows.size
+    }
+
+    public getWindowIds(): AvailableIds[number][] {
+        return Array.from(this.windows.keys())
+    }
+
+    public executeOnWindow<T>(id: AvailableIds[number], callback: (window: BrowserWindow) => T): T | undefined {
+        const win = this.windows.get(id)
+        if (win && !win.isDestroyed()) {
+            return callback(win)
+        }
+        return undefined
+    }
+}
+
+type WindowId = Array<string | number>
+const windowManager = new WindowManager<WindowId, ['plugin']>()
+export { windowManager }
